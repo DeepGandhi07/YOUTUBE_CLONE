b1209f40bb36e8b8942fe613147baf29aa534577
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-02-14 11:41:38 +0530

    Commit

---
+++ src/context/contextApi.js
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+
+const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState(false);
+  const [selectCategories, setSelectCategories] = useState("New");
+  const [MobileMenu, setMobileMenu] = useState(false);
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+      console.log(contents);
+      searchResults(contents);
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategories);
+  }, [selectCategories]);
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResults,
+        setSearchResults,
+        selectCategories,
+        setSelectCategories,
+        MobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
