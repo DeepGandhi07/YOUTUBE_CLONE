7f37643af1f1a48c15572dbca7370ab4e571e12d
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-02-16 13:04:55 +0530

    Commit

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -6,7 +6,7 @@ import { categories } from "../utils/constants";
 import { Context } from "../context/contextApi";
 
 const LeftNav = () => {
-  const { selectCategories, setSelectCategories, mobileMenu } =
+  const { selectedCategory, setSelectedCategory, mobileMenu } =
     useContext(Context);
 
   const navigate = useNavigate();
@@ -14,9 +14,9 @@ const LeftNav = () => {
   const clickHandler = (name, type) => {
     switch (type) {
       case "category":
-        return setSelectCategories(name);
+        return setSelectedCategory(name);
       case "home":
-        return setSelectCategories(name);
+        return setSelectedCategory(name);
       case "menu":
         return false;
       default:
@@ -27,7 +27,7 @@ const LeftNav = () => {
   return (
     <div
       className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
-        mobileMenu ? "translate-x-0.125rem" : ""
+        mobileMenu ? "translate-x-[0px]" : ""
       }`}
     >
       <div className="flex px-5 flex-col">
@@ -42,7 +42,7 @@ const LeftNav = () => {
                   navigate("/");
                 }}
                 className={`${
-                  selectCategories === item.name ? "bg-white/[0.15]" : ""
+                  selectedCategory === item.name ? "bg-white/[0.15]" : ""
                 }`}
               />
               {item.divider && <hr className="my-5 border-white/[0.2]" />}
@@ -51,7 +51,7 @@ const LeftNav = () => {
         })}
         <hr className="my-5 border-white/[0.2]" />
         <div className="text-white/[0.5] text-[12px]">
-          <a href="https://deepgandhi.netlify.app/">Clone by: Deep Gandhi</a>
+          Clone by: JS Dev Hindi
         </div>
       </div>
     </div>
